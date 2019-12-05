ad08921fe84eac02e7a708b03e41c0586a25cd1b
Juan Carlos Vidal <juancarvp@gmail.com>
2019-12-04 19:37:58 -0500

    Updates

---
+++ precache-manifest.be63b234b68ef101ed4e1f38dd290dff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc9b1d5737f8391e3f81f800db8d049",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "a47e4ad965e5c30b6c28",
+    "url": "/test1/static/css/main.cff729f3.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/test1/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "a47e4ad965e5c30b6c28",
+    "url": "/test1/static/js/main.05e29e71.chunk.js"
+  },
+  {
+    "revision": "110df05cd91b572a9bfe",
+    "url": "/test1/static/js/runtime~main.044b3335.js"
+  }
+]);
